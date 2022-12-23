c71ad313464fda98129363a7403260d202a9fbb3
Stanislav <stanislaworonin@yandex.ru>
2022-12-23 14:17:49 +0300

    add delete all rout for sql

--- src/modules/testing/testingController.ts
+++ src/modules/testing/testingController.ts
@@ -6,26 +6,38 @@ import { DataSource } from "typeorm";
 export class TestingController {
   constructor(@InjectDataSource() private dataSource: DataSource) {
   }
+
   @Delete('all-data')
-  @HttpCode(204)
   async deleteAll() {
     await this.dataSource.query(`
-      CREATE OR REPLACE FUNCTION truncate_tables(username in VARCHAR) RETURNS void AS $$
-      DECLARE
-      statements CURSOR FOR
-       SELECT tablename FROM pg_tables
-        WHERE tableowner = username AND schemaname = 'public';
-        BEGIN
-          FOR stmt IN statements LOOP
-          EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;'
-              END LOOP;
-              END;
-               $$ LANGUAGE plpgsql;
-                  SELECT truncate_tables('postgres');
-    `);
-
-    return;
+      DELETE FROM users
+      DELETE FROM user_ban_info
+      DELETE FROM device_security
+      DELETE FROM email_confirmation
+      DELETE FROM token_black_list
+    `)
   }
+
+  // @Delete('all-data')
+  // @HttpCode(204)
+  // async deleteAll() {
+  //   await this.dataSource.query(`
+  //     CREATE OR REPLACE FUNCTION truncate_tables(username in VARCHAR) RETURNS void AS $$
+  //     DECLARE
+  //     statements CURSOR FOR
+  //      SELECT tablename FROM pg_tables
+  //       WHERE tableowner = username AND schemaname = 'public';
+  //       BEGIN
+  //         FOR stmt IN statements LOOP
+  //         EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;'
+  //             END LOOP;
+  //             END;
+  //              $$ LANGUAGE plpgsql;
+  //                 SELECT truncate_tables('postgres');
+  //   `);
+
+  //   return;
+  // }
 
   // @Delete('all-data')
   // @HttpCode(204)
